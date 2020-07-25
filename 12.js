c1acfdefd9b9eb7167c22404a4826f8a696e2a66
nabilaafifahqotrunnada <nabila_afifah_28rpl@student.smktelkom-mlg.sch.id>
2020-07-25 23:13:52 +0700

    Second Commit

---
+++ 12.js
@@ -0,0 +1,13 @@
+//Percabangan If.. Else..
+
+//mengisi variabel tahun dengan data numerik
+let tahun = 2020
+
+//kondisi else hanya akan dieksekusi bila kondisi if bernilai false
+if(tahun % 4 == 0){
+    console.log(tahun + " adalah tahun kabisat");
+}else{
+    console.log(tahun + " bukan tahun kabisat");
+}
+/*karena 2020 dibagi 4 sisa 0 dan 
+statement if bernilai true maka kondisi else tidak dieksekusi*/
